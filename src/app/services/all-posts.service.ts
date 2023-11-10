8571cedb37f313a71b39b5f4cf6a5c943616be5e
LudvigOrest <ludvig.orest@live.se>
2023-11-10 18:38:29 +0800

    added some simple styling to visualise the array of posts in post-service (in skate-view)

--- src/app/services/all-posts.service.ts
+++ src/app/services/all-posts.service.ts
@@ -8,12 +8,13 @@ import { CurrentUserService } from './current-user.service';
 })
 export class AllPostsService {
 
+  placeholderUrl: string = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallsdesk.com%2Fwp-content%2Fuploads%2F2017%2F01%2FPictures-of-Skateboarding.jpg&f=1&nofb=1&ipt=392677f657969c327b047f53e6bea054e928eeeb4ec07b85dee6389f68160cf7&ipo=images";
   allPosts: Blogpost[] = [];
   testUser: User = new User("testnamn", "lösen", false);
 
   constructor() {
-    this.allPosts.push(new Blogpost("headerTest", "imgUrl", this.testUser, ["tags", "tag2"], "blabla"));
-    this.allPosts.push(new Blogpost("headerTest2", "imgUrl", this.testUser, ["tags2", "tag3"], "blabla2"));
+    this.allPosts.push(new Blogpost("headerTest", this.placeholderUrl, this.testUser, ["tags", "tag2"], "blabla"));
+    this.allPosts.push(new Blogpost("headerTest2", this.placeholderUrl, this.testUser, ["tags2", "tag3"], "blabla2"));
     console.log(this.allPosts);
   }
 }
